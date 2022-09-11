ff47de15d4b1cae90e880f1bf0412c95e99c71f5
WinWinter <akeraanucha1@gmail.com>
2022-09-11 12:41:50 +0700

    2

---
+++ weekly/week2-2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,i=0;
+    scanf("%d",&a);
+    while (1==1)
+    {
+        if (i<a)
+        {
+            printf("*");
+            i++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    
+    
+    return 0;
+}
